bdecd9f365e36e4b56a7f1887667271865077530
Chris Patuzzo <chris@patuzzo.co.uk>
2021-10-03 12:25:26 +0100

    Rename the token's symbol from CARD to WSUN

--- contracts/PuzzleCard.sol
+++ contracts/PuzzleCard.sol
@@ -7,7 +7,7 @@ import "./vendor/ERC721Tradable.sol";
 contract PuzzleCard is ERC721Tradable {
     string private currentBaseTokenURI;
 
-    constructor(address proxyAddress) ERC721Tradable("PuzzleCard", "CARD", proxyAddress) {
+    constructor(address proxyAddress) ERC721Tradable("PuzzleCard", "WSUN", proxyAddress) {
         setBaseTokenURI("https://cd6c-2a02-6b6c-60-0-cdb2-1b9f-aa0f-454f.ngrok.io/api/");
     }
 
