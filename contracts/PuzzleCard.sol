47630acde1e6627f7a1946501c0331b7935e38da
Chris Patuzzo <chris@patuzzo.co.uk>
2021-10-03 12:09:18 +0100

    Start populating off-chain metadata for presentation on OpenSea
    I plan to pre-generate all metadata and serve it statically from the
    current repository for simplicity.

--- contracts/PuzzleCard.sol
+++ contracts/PuzzleCard.sol
@@ -8,6 +8,6 @@ contract PuzzleCard is ERC721Tradable {
     constructor(address _proxyRegistryAddress) ERC721Tradable("PuzzleCard", "CARD", _proxyRegistryAddress) {}
 
     function baseTokenURI() override public pure returns (string memory) {
-        return "https://example.com/api";
+        return "https://cd6c-2a02-6b6c-60-0-cdb2-1b9f-aa0f-454f.ngrok.io/api/";
     }
 }
