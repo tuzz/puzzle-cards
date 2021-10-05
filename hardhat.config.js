294d0083b3c70bd65f8b56a9254d7d098a747f8b
Chris Patuzzo <chris@patuzzo.co.uk>
2021-10-05 14:02:43 +0100

    Enable the solidity optimizer

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,15 @@ const system = (command) => require("child_process").execSync(command).toString(
 const privateKey = system("gpg --decrypt ~/Dropbox/Secrets/metamask/private-key.gpg 2>/dev/null");
 
 module.exports = {
-  solidity: "0.8.4",
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
   networks: {
     hardhat: {
       accounts: {
