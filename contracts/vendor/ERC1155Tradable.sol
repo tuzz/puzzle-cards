e45aef2d752120018ec33b98c2035b85a300b9b5
Chris Patuzzo <chris@patuzzo.co.uk>
2021-10-11 10:09:15 +0100

    Remove ‘creators’ from the ERC1155 contract

--- contracts/vendor/ERC1155Tradable.sol
+++ contracts/vendor/ERC1155Tradable.sol
@@ -26,21 +26,12 @@ contract ERC1155Tradable is ContextMixin, ERC1155, NativeMetaTransaction, Ownabl
   using SafeMath for uint256;
 
   address proxyRegistryAddress;
-  mapping (uint256 => address) public creators;
   mapping (uint256 => uint256) internal tokenSupply;
   mapping (uint256 => string) customUri;
   // Contract name
   string public name;
   // Contract symbol
   string public symbol;
-
-  /**
-   * @dev Require _msgSender() to be the creator of the token id
-   */
-  modifier creatorOnly(uint256 _id) {
-    require(creators[_id] == _msgSender(), "ERC1155Tradable#creatorOnly: ONLY_CREATOR_ALLOWED");
-    _;
-  }
 
   /**
    * @dev Require _msgSender() to own more than 0 of the token id
@@ -106,48 +97,9 @@ contract ERC1155Tradable is ContextMixin, ERC1155, NativeMetaTransaction, Ownabl
   function setCustomURI(
     uint256 _tokenId,
     string memory _newURI
-  ) public creatorOnly(_tokenId) {
+  ) public onlyOwner {
     customUri[_tokenId] = _newURI;
     emit URI(_newURI, _tokenId);
-  }
-
-  /**
-    * @dev Creates a new token type and assigns _initialSupply to an address
-    * NOTE: remove onlyOwner if you want third parties to create new tokens on
-    *       your contract (which may change your IDs)
-    * NOTE: The token id must be passed. This allows lazy creation of tokens or
-    *       creating NFTs by setting the id's high bits with the method
-    *       described in ERC1155 or to use ids representing values other than
-    *       successive small integers. If you wish to create ids as successive
-    *       small integers you can either subclass this class to count onchain
-    *       or maintain the offchain cache of identifiers recommended in
-    *       ERC1155 and calculate successive ids from that.
-    * @param _initialOwner address of the first owner of the token
-    * @param _id The id of the token to create (must not currenty exist).
-    * @param _initialSupply amount to supply the first owner
-    * @param _uri Optional URI for this token type
-    * @param _data Data to pass if receiver is contract
-    * @return The newly created token ID
-    */
-  function create(
-    address _initialOwner,
-    uint256 _id,
-    uint256 _initialSupply,
-    string memory _uri,
-    bytes memory _data
-  ) public onlyOwner returns (uint256) {
-    require(!_exists(_id), "token _id already exists");
-    creators[_id] = _msgSender();
-
-    if (bytes(_uri).length > 0) {
-      customUri[_id] = _uri;
-      emit URI(_uri, _id);
-    }
-
-    _mint(_initialOwner, _id, _initialSupply, _data);
-
-    tokenSupply[_id] = _initialSupply;
-    return _id;
   }
 
   /**
@@ -202,22 +154,6 @@ contract ERC1155Tradable is ContextMixin, ERC1155, NativeMetaTransaction, Ownabl
   }
 
   /**
-    * @dev Change the creator address for given tokens
-    * @param _to   Address of the new creator
-    * @param _ids  Array of Token IDs to change creator
-    */
-  function setCreator(
-    address _to,
-    uint256[] memory _ids
-  ) public {
-    require(_to != address(0), "ERC1155Tradable#setCreator: INVALID_ADDRESS.");
-    for (uint256 i = 0; i < _ids.length; i++) {
-      uint256 id = _ids[i];
-      _setCreator(_to, id);
-    }
-  }
-
-  /**
    * Override isApprovedForAll to whitelist user's OpenSea proxy accounts to enable gas-free listings.
    */
   function isApprovedForAll(
@@ -234,16 +170,6 @@ contract ERC1155Tradable is ContextMixin, ERC1155, NativeMetaTransaction, Ownabl
   }
 
   /**
-    * @dev Change the creator address for given token
-    * @param _to   Address of the new creator
-    * @param _id  Token IDs to change creator of
-    */
-  function _setCreator(address _to, uint256 _id) internal creatorOnly(_id)
-  {
-      creators[_id] = _to;
-  }
-
-  /**
     * @dev Returns whether the specified token exists by checking to see if it has a creator
     * @param _id uint256 ID of the token to query the existence of
     * @return bool whether the token exists
@@ -251,7 +177,7 @@ contract ERC1155Tradable is ContextMixin, ERC1155, NativeMetaTransaction, Ownabl
   function _exists(
     uint256 _id
   ) internal view returns (bool) {
-    return creators[_id] != address(0);
+    return tokenSupply[_id] != 0;
   }
 
   function exists(
