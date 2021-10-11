3ec10e514b312c6d9e3e9f51d10b961b0487be0d
Chris Patuzzo <chris@patuzzo.co.uk>
2021-10-11 10:56:51 +0100

    Tidy up ERC1155Tradabole.sol

--- contracts/vendor/ERC1155Tradable.sol
+++ contracts/vendor/ERC1155Tradable.sol
@@ -4,8 +4,6 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
 
 import "./ContentMixin.sol";
 import "./NativeMetaTransaction.sol";
@@ -16,145 +14,66 @@ contract ProxyRegistry {
   mapping(address => OwnableDelegateProxy) public proxies;
 }
 
-/**
- * @title ERC1155Tradable
- * ERC1155Tradable - ERC1155 contract that whitelists an operator address, has create and mint functionality, and supports useful standards from OpenZeppelin,
-  like _exists(), name(), symbol(), and totalSupply()
- */
 contract ERC1155Tradable is ContextMixin, ERC1155, NativeMetaTransaction, Ownable {
-  using Strings for string;
-  using SafeMath for uint256;
-
   address proxyRegistryAddress;
   mapping (uint256 => uint256) internal tokenSupply;
-  // Contract name
   string public name;
-  // Contract symbol
   string public symbol;
 
-  /**
-   * @dev Require _msgSender() to own more than 0 of the token id
-   */
-  modifier ownersOnly(uint256 _id) {
-    require(balanceOf(_msgSender(), _id) > 0, "ERC1155Tradable#ownersOnly: ONLY_OWNERS_ALLOWED");
-    _;
+  constructor(string memory _name, string memory _symbol, address _proxyRegistryAddress) ERC1155("") {
+      name = _name;
+      symbol = _symbol;
+      proxyRegistryAddress = _proxyRegistryAddress;
+      _initializeEIP712(name);
   }
 
-  constructor(
-    string memory _name,
-    string memory _symbol,
-    address _proxyRegistryAddress
-  ) ERC1155("") {
-    name = _name;
-    symbol = _symbol;
-    proxyRegistryAddress = _proxyRegistryAddress;
-    _initializeEIP712(name);
+  function totalSupply(uint256 _id) public view returns (uint256) {
+      return tokenSupply[_id];
   }
 
-  /**
-    * @dev Returns the total quantity for a token ID
-    * @param _id uint256 ID of the token to query
-    * @return amount of token in existence
-    */
-  function totalSupply(
-    uint256 _id
-  ) public view returns (uint256) {
-    return tokenSupply[_id];
+  function mint(address _to, uint256 _id, uint256 _quantity, bytes memory _data) virtual internal {
+      _mint(_to, _id, _quantity, _data);
+      tokenSupply[_id] += _quantity;
   }
 
-  /**
-    * @dev Mints some amount of tokens to an address
-    * @param _to          Address of the future owner of the token
-    * @param _id          Token ID to mint
-    * @param _quantity    Amount of tokens to mint
-    * @param _data        Data to pass if receiver is contract
-    */
-  function mint(
-    address _to,
-    uint256 _id,
-    uint256 _quantity,
-    bytes memory _data
-  ) virtual internal {
-    _mint(_to, _id, _quantity, _data);
-    tokenSupply[_id] = tokenSupply[_id].add(_quantity);
+  function batchMint(address _to, uint256[] memory _ids, uint256[] memory _quantities, bytes memory _data) internal {
+      for (uint256 i = 0; i < _ids.length; i++) {
+        uint256 _id = _ids[i];
+        uint256 quantity = _quantities[i];
+        tokenSupply[_id] += quantity;
+      }
+
+      _mintBatch(_to, _ids, _quantities, _data);
   }
 
-  /**
-    * @dev Mint tokens for each id in _ids
-    * @param _to          The address to mint tokens to
-    * @param _ids         Array of ids to mint
-    * @param _quantities  Array of amounts of tokens to mint per id
-    * @param _data        Data to pass if receiver is contract
-    */
-  function batchMint(
-    address _to,
-    uint256[] memory _ids,
-    uint256[] memory _quantities,
-    bytes memory _data
-  ) internal {
-    for (uint256 i = 0; i < _ids.length; i++) {
-      uint256 _id = _ids[i];
-      uint256 quantity = _quantities[i];
-      tokenSupply[_id] = tokenSupply[_id].add(quantity);
-    }
-    _mintBatch(_to, _ids, _quantities, _data);
+  function batchBurn(address _to, uint256[] memory _ids, uint256[] memory _quantities) internal {
+      for (uint256 i = 0; i < _ids.length; i++) {
+        uint256 _id = _ids[i];
+        uint256 quantity = _quantities[i];
+        tokenSupply[_id] -= quantity;
+      }
+
+      _burnBatch(_to, _ids, _quantities);
   }
 
-  function batchBurn(
-    address _to,
-    uint256[] memory _ids,
-    uint256[] memory _quantities
-  ) internal {
-    for (uint256 i = 0; i < _ids.length; i++) {
-      uint256 _id = _ids[i];
-      uint256 quantity = _quantities[i];
-      tokenSupply[_id] = tokenSupply[_id].sub(quantity);
-    }
-    _burnBatch(_to, _ids, _quantities);
+  // Override isApprovedForAll to whitelist user's OpenSea proxy accounts to enable gas-free listings.
+  function isApprovedForAll(address _owner, address _operator) override public view returns (bool isOperator) {
+      ProxyRegistry proxyRegistry = ProxyRegistry(proxyRegistryAddress);
+
+      if (address(proxyRegistry.proxies(_owner)) == _operator) { return true; }
+
+      return ERC1155.isApprovedForAll(_owner, _operator);
   }
 
-  /**
-   * Override isApprovedForAll to whitelist user's OpenSea proxy accounts to enable gas-free listings.
-   */
-  function isApprovedForAll(
-    address _owner,
-    address _operator
-  ) override public view returns (bool isOperator) {
-    // Whitelist OpenSea proxy contract for easy trading.
-    ProxyRegistry proxyRegistry = ProxyRegistry(proxyRegistryAddress);
-    if (address(proxyRegistry.proxies(_owner)) == _operator) {
-      return true;
-    }
-
-    return ERC1155.isApprovedForAll(_owner, _operator);
+  function _exists(uint256 _id) internal view returns (bool) {
+      return tokenSupply[_id] != 0;
   }
 
-  /**
-    * @dev Returns whether the specified token exists by checking to see if it has a creator
-    * @param _id uint256 ID of the token to query the existence of
-    * @return bool whether the token exists
-    */
-  function _exists(
-    uint256 _id
-  ) internal view returns (bool) {
-    return tokenSupply[_id] != 0;
+  function exists(uint256 _id) external view returns (bool) {
+      return _exists(_id);
   }
 
-  function exists(
-    uint256 _id
-  ) external view returns (bool) {
-    return _exists(_id);
+  function _msgSender() internal override view returns (address sender) {
+      return ContextMixin.msgSender();
   }
-
-    /**
-     * This is used instead of msg.sender as transactions won't be sent by the original token owner, but by OpenSea.
-     */
-    function _msgSender()
-        internal
-        override
-        view
-        returns (address sender)
-    {
-        return ContextMixin.msgSender();
-    }
 }
