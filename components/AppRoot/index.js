6c05cc10ca4dc3e844509ac2fcbdd611ae1999e4
Chris Patuzzo <chris@patuzzo.co.uk>
2021-10-19 10:23:29 +0100

    Extract pages/_app.js into an AppRoot component

---
+++ components/AppRoot/index.js
@@ -0,0 +1,77 @@
+import { useState, useEffect, createContext } from "react";
+import { ethers } from "ethers";
+import PuzzleCard from "../../public/PuzzleCard";
+import AppContext from "../AppContext";
+
+const AppRoot = ({ Component, pageProps }) => {
+  const [appContext, setAppContext] = useState({ PuzzleCard, decks: {} });
+  const [connectPoller, setConnectPoller] = useState();
+
+  useEffect(async () => {
+    if (typeof ethereum === "undefined") { return; }
+
+    const provider = new ethers.providers.Web3Provider(ethereum, "any");
+    const signer = provider.getSigner();
+
+    PuzzleCard.attach(ethers, provider);
+    PuzzleCard.connect(signer);
+
+    const address = await signer.getAddress().catch(() => {});
+    if (address) { ensureDeck(address); } else { pollForConnect(); }
+
+    ethereum.on("accountsChanged", ([address]) => address && ensureDeck(address));
+  }, []);
+
+  const pollForConnect = () => {
+    setConnectPoller(setInterval(() => {
+      PuzzleCard.CONTRACT.signer.getAddress().then(ensureDeck).catch(() => {});
+    }, 1000));
+  };
+
+  const ensureDeck = async (address) => {
+    address = address.toLowerCase();
+
+    setAppContext(c => {
+      if (c.decks[address]) {
+        return { ...c, address };
+      } else {
+        return { ...c, address, decks: { ...c.decks, [address]: [] } };
+      }
+    });
+
+    setConnectPoller(poller => { poller && clearInterval(poller); });
+  };
+
+  useEffect(() => {
+    for (let [address, deck] of Object.entries(appContext.decks)) {
+      if (!deck.fetching && !deck.fetched) {
+        deck.fetching = true;
+
+        PuzzleCard.fetchDeck(address, updateFetchedDeck(address), console.log).then(deck => {
+          deck.fetched = true;
+
+          setAppContext(c => ({ ...c, decks: { ...c.decks, [address]: deck } }));
+        });
+      }
+    }
+  }, [appContext.decks]);
+
+  const updateFetchedDeck = (address) => (changes) => {
+    setAppContext(c => {
+      const deck = c.decks[address].slice(0);
+      deck.fetched = true;
+
+      PuzzleCard.updateFetchedDeck(deck, changes);
+
+      return { ...c, decks: { ...c.decks, [address]: deck } };
+    });
+  };
+
+  return (
+    <AppContext.Provider value={appContext}>
+      <Component {...pageProps} />
+    </AppContext.Provider>
+  );
+};
+
+export default AppRoot;
