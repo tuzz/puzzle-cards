e4b688fa4f0132302878c1de69ea041151489167
Chris Patuzzo <chris@patuzzo.co.uk>
2021-10-19 17:49:47 +0100

    Fix a horizontal scrollbar showing because of the yellow sun

--- components/YellowSun/index.jsx
+++ components/YellowSun/index.jsx
@@ -1,8 +1,10 @@
 import styles from "./styles.module.scss";
 
 const YellowSun = ({ className }) => (
-  <div className={`${styles.yellow_sun} ${className}`}>
-    <img src="/images/yellow_sun.png" />
+  <div className={styles.hide_overflow}>
+    <div className={`${styles.yellow_sun} ${className}`}>
+      <img src="/images/yellow_sun.png" />
+    </div>
   </div>
 );
 
