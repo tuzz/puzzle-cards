81c72f2097e5c2dd75af2ccb26d31bf340eb45fc
Chris Patuzzo <chris@patuzzo.co.uk>
2021-10-19 14:13:21 +0100

    Add an optional rock pillar below the worship stick

--- components/WorshipStick/index.jsx
+++ components/WorshipStick/index.jsx
@@ -1,10 +1,14 @@
 import styles from "./styles.module.scss";
 
-const WorshipStick = ({ className, spinning = true }) => {
+const WorshipStick = ({ className, spinning = true, rockHeight = 1 }) => {
+  const paddingTop = `${rockHeight * 100}%`;
+
   return (
     <div className={`${styles.worship_stick} ${className}`}>
       <img src="/images/worship_stick_base.png" className={styles.base} />
       <img src="/images/worship_stick_sun.png" className={`${styles.sun} ${!spinning && styles.paused}`} />
+
+      {rockHeight > 0 && <div className={styles.rock} style={{ paddingTop }}></div>}
     </div>
   );
 };
