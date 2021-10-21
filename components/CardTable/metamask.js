4bc726420a575a6a3498950be289363b2c101b01
Chris Patuzzo <chris@patuzzo.co.uk>
2021-10-21 19:15:45 +0100

    Make the Metamask.performAction flow less confusing for the user

--- components/CardTable/metamask.js
+++ components/CardTable/metamask.js
@@ -9,32 +9,88 @@ Metamask.actionsThatCanBeTaken = async (PuzzleCard, cards) => {
     return [];
   }
 
-  if (!await Metamask.ensureCorrectNetwork(PuzzleCard)) {
-    alert("Please connect to the Polygon network to use this website.");
-    return [];
-  }
+  // TODO: similar pattern to below
 
   return PuzzleCard.actionsThatCanBeTaken(cards);
 };
 
+// Returns an array containing two elements:
+//   0: whether the action was initiated on the network
+//   1: a promise that will return whether the action was successful
+// TODO: make this method support cardStacks and return success if *any* of the
+// promises succeeds - this is so that the user can choose how many to combine.
 Metamask.performAction = async (PuzzleCard, actionName, cards) => {
+  Metamask.tryAgain = false;
+
   if (typeof ethereum === "undefined") {
     alert("Please install the MetaMask browser extension to use this website.");
-    return false;
+    return [false, false];
   }
 
-  if (!await Metamask.ensureCorrectNetwork(PuzzleCard)) {
-    return false;
+  if (actionName !== "connectToMetamask" && await Metamask.alreadyConnected(PuzzleCard) && await Metamask.alreadyCorrectNetwork(PuzzleCard)) {
+    return [true, PuzzleCard[actionName](cards).then(() => true).catch(() => false)];
   }
 
-  if (actionName === "connectToMetamask") {
-    return await Metamask.connectOrReloadAndConnect();
+  // Only allow the page to reload if the user won't lose the positions of their cards.
+  const isOkToReload = actionName === "connectToMetamask";
+
+  if (isOkToReload && await Metamask.ensureConnectedOrReloadPageToShowPromptAgain()) {
+    return [true, true];
   }
 
-  return PuzzleCard[actionName](cards).then(() => true).catch(() => false);
+  // This will fail if there is already a prompt to connect that the user hasn't yet accepted.
+  const managedToConnect = await Metamask.connect();
+
+  // Prompt the user to first accept the original prompt then press the button
+  // again to reinitiate the request. Otherwise, no popup is created for the
+  // request and it's confusing to the user that there's an Unnapproved request
+  // sitting in MetaMask that they would need to accept. Even with this flow,
+  // MetaMask still prompts the user to log in a second time after pressing on
+  // the button which is annoying but at least it's obvious what to do.
+  if (!managedToConnect) {
+    alert("Please unlock MetaMask then press the button again.");
+    Metamask.tryAgain = true;
+    return [false, false]
+  } else if (Metamask.tryAgain) {
+    return [false, false];
+  }
+
+  // This follows the same pattern as above. The slight difference is that we
+  // should only show the alert when the switch network promise is unresolved.
+  // We don't want to show it if the user just cancels because then it's apparent
+  // when the action wasn't performed. We don't need to do this check for
+  // connecting because there's no cancel button on the MetaMask unlock screen.
+  const managedToSwitch = await Metamask.switchNetwork(PuzzleCard);
+
+  if (!managedToSwitch) {
+    if (Metamask.promptingToSwitch) {
+      alert("Please allow MetaMask to switch network then press the button again.");
+    }
+    Metamask.tryAgain = true;
+    return [false, false]
+  } else if (Metamask.tryAgain) {
+    return [false, false];
+  }
+
+  // Initiate the request immediate after connect/switching since a popup will be shown.
+  return [true, PuzzleCard[actionName](cards).then(() => true).catch(() => false)];
 };
 
-Metamask.connectOrReloadAndConnect = async () => {
+Metamask.alreadyConnected = async (PuzzleCard) => {
+  const address = await PuzzleCard.CONTRACT.signer.getAddress().catch(() => {});
+  return !!address;
+};
+
+Metamask.connect = async () => {
+  try {
+    await ethereum.request({ method: "eth_requestAccounts" });
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+Metamask.ensureConnectedOrReloadPageToShowPromptAgain = async () => {
   try {
     await ethereum.request({ method: "eth_requestAccounts" });
     return true;
@@ -48,15 +104,21 @@ Metamask.connectOrReloadAndConnect = async () => {
     const delimiter = href.match(/\?/) ? "&" : "?";
     window.location.href += delimiter + "connectOnLoad=true";
 
-    return false; // Kind of unreachable because the page reloads.
+    return true; // Kind of unreachable because the page reloads.
   }
 };
 
-Metamask.ensureCorrectNetwork = async (PuzzleCard) => {
+Metamask.alreadyCorrectNetwork = async (PuzzleCard) => {
   const expectedNetwork = PuzzleCard.CONTRACT_NETWORK;
   const actualNetwork = await PuzzleCard.CONTRACT.provider.getNetwork();
 
-  if (actualNetwork.chainId === expectedNetwork.chainId) { return true; }
+  return actualNetwork.chainId === expectedNetwork.chainId;
+};
+
+Metamask.switchNetwork = async (PuzzleCard) => {
+  const expectedNetwork = PuzzleCard.CONTRACT_NETWORK;
+
+  Metamask.promptingToSwitch = true;
 
   await ethereum.request({ method: "wallet_addEthereumChain", params: [{
     chainName: expectedNetwork.name,
@@ -64,7 +126,7 @@ Metamask.ensureCorrectNetwork = async (PuzzleCard) => {
     rpcUrls: [expectedNetwork.url],
     nativeCurrency: { symbol: expectedNetwork.symbol, decimals: 18 },
     blockExplorerUrls: [expectedNetwork.explorer],
-  }] });
+  }] }).then(() => Metamask.promptingToSwitch = false).catch(() => {});
 
   const updatedNetwork = await PuzzleCard.CONTRACT.provider.getNetwork();
   return updatedNetwork.chainId == expectedNetwork.chainId;
