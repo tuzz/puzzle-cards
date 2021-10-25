7ff81142fa56d11ccb3aed52509449c3e6ab3706
Chris Patuzzo <chris@patuzzo.co.uk>
2021-10-25 11:04:41 +0100

    Ensure updateStackPositions operates on the most recent stackPositions
    Use the argument of the setStackPositions call rather than the
    stackPositions state variable which might be stale if other asynchronous
    calls are running at the same time.

--- components/CardsInPlay/index.jsx
+++ components/CardsInPlay/index.jsx
@@ -7,8 +7,8 @@ import styles from "./styles.module.scss";
 const CardsInPlay = ({ onStackMoved = () => {}, buttonFlashing }) => {
   const { address, decks } = useContext(AppContext);
   const [loadedAddress, setLoadedAddress] = useState(address);
-  const [stackPositions, setStackPositions] = useState([]);
-  const [batchTokenIDs, setBatchTokenIDs] = useState(new Set());
+  const [stackPositions, _setStackPositions] = useState(withBatchTokenIDs([]));
+  const setStackPositions = (array) => _setStackPositions(withBatchTokenIDs(array));
 
   useEffect(() => {
     if (!address) { return; }
@@ -33,63 +33,71 @@ const CardsInPlay = ({ onStackMoved = () => {}, buttonFlashing }) => {
 
   // Keep track of which tokenIDs have been minted since the button started flashing.
   // This is so we can position the new card stacks in a rotated fan on top of each other.
-  useEffect(() => buttonFlashing && setBatchTokenIDs([]), [buttonFlashing]);
+  useEffect(() => {
+    if (buttonFlashing) {
+      setStackPositions(array => { array.batchTokenIDs = new Set(); return array; });
+    }
+  }, [buttonFlashing]);
 
   const updateStackPositions = (cardStacks) => {
-    const newStackPositions = [...stackPositions]; // TODO: try to write with setState in case of races
-    const newBatchTokenIDs = new Set(batchTokenIDs);
+    // TODO: explicitly check if we're minting so it can be handled differently
+    if (cardStacks.justChanged.length > 2) { return; }
 
-    const pageMiddle = document.body.clientWidth / 2;
+    setStackPositions(stackPositions => {
+      const newStackPositions = [...stackPositions];
+      const pageMiddle = document.body.clientWidth / 2;
 
-    // Update quantities. If a stack is depleted, remove its stackPosition and
-    // inform the parent that it has 'moved' so the parent can remove it too.
-    for (let cardStack of cardStacks.justChanged) {
-      const index = newStackPositions.findIndex(p => p.cardStack.tokenID === cardStack.tokenID);
-      const visible = index !== -1;
+      newStackPositions.batchTokenIDs = stackPositions.batchTokenIDs;
 
-      if (visible && cardStack.quantity === 0) {
-        newStackPositions.splice(index, 1);
-        onStackMoved({ cardStack, movedTo: null }); // The void.
-      } else if (visible) {
-        // This change seems to be visible to the parent component which is good.
-        newStackPositions[index].cardStack.quantity = cardStack.quantity;
-      }
+      // Update quantities. If a stack is depleted, remove its stackPosition and
+      // inform the parent that it has 'moved' so the parent can remove it too.
+      for (let cardStack of cardStacks.justChanged) {
+        const index = newStackPositions.findIndex(p => p.cardStack.tokenID === cardStack.tokenID);
+        const visible = index !== -1;
 
-      // If a card was added, make the stack appear over the CardOutline. If the
-      // stack is already visible, force a re-render by changing its key.
-      if (cardStack.quantity > 0 && cardStack.lastDelta > 0) {
-        const left = pageMiddle - layout.stackWidth / 2;
-        const top = layout.outlineTop;
-
-        // Fan the cards out if lots are minted but don't go outside the card outline.
-        const numMinted = newBatchTokenIDs.size;
-        const fanAngle = 3 * numMinted;
-        const fanOffset = Math.min(10 * numMinted, layout.stackWidth - 50);
-
-        // TODO: try to set z-index so fan is stacked in the expected order
-        // TODO: don't flip over cards after the first?
-
-        const rotation = { degrees: 0, random: 4, initial: fanAngle };
-        const position = { left: left + fanOffset, top, rotation };
-        const fadeIn = false;
-
-        if (visible) {
-          const existing = newStackPositions[index];
-
-          existing.position = position;
-          existing.generation = (existing.generation || 0) + 1;
-          existing.fadeIn = fadeIn;
-        } else {
-          newStackPositions.splice(0, 0, { cardStack, position, fadeIn });
+        if (visible && cardStack.quantity === 0) {
+          newStackPositions.splice(index, 1);
+          setTimeout(() => onStackMoved({ cardStack, movedTo: null }), 0); // The void.
+        } else if (visible) {
+          // This change seems to be visible to the parent component which is good.
+          newStackPositions[index].cardStack.quantity = cardStack.quantity;
         }
 
-        onStackMoved({ cardStack, movedTo: { cardOutline: true } });
-        newBatchTokenIDs.add(cardStack.tokenID);
-      }
-    }
+        // If a card was added, make the stack appear over the CardOutline. If the
+        // stack is already visible, force a re-render by changing its key.
+        if (cardStack.quantity > 0 && cardStack.lastDelta > 0) {
+          const left = pageMiddle - layout.stackWidth / 2;
+          const top = layout.outlineTop;
 
-    setStackPositions(newStackPositions);
-    setBatchTokenIDs(newBatchTokenIDs);
+          // Fan the cards out if lots are minted but don't go outside the card outline.
+          const numMinted = newStackPositions.batchTokenIDs.size;
+          const fanAngle = 2 * numMinted;
+          const fanOffset = Math.min(5 * numMinted, layout.stackWidth - 50);
+
+          // TODO: try to set z-index so fan is stacked in the expected order
+          // TODO: don't flip over cards after the first?
+
+          const rotation = { degrees: 0, random: 4, initial: fanAngle };
+          const position = { left: left + fanOffset, top, rotation };
+          const fadeIn = false;
+
+          if (visible) {
+            const existing = newStackPositions[index];
+
+            existing.position = position;
+            existing.generation = (existing.generation || 0) + 1;
+            existing.fadeIn = fadeIn;
+          } else {
+            newStackPositions.splice(0, 0, { cardStack, position, fadeIn });
+          }
+
+          setTimeout(() => onStackMoved({ cardStack, movedTo: { cardOutline: true } }), 0);
+          newStackPositions.batchTokenIDs.add(cardStack.tokenID);
+        }
+      }
+
+      return newStackPositions;
+    });
   };
 
   const key = (stackPosition) => `${stackPosition.cardStack.tokenID}-${stackPosition.generation}`;
@@ -102,5 +110,14 @@ const CardsInPlay = ({ onStackMoved = () => {}, buttonFlashing }) => {
     </div>
   );
 };
+
+
+const withBatchTokenIDs = (array) => {
+  if (!array.batchTokenIDs) {
+    array.batchTokenIDs = new Set();
+  }
+
+  return array;
+}
 
 export default CardsInPlay;
