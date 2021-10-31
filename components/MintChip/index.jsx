8b310fc2f7fad55fde0a9ccf6d6926e239dfc942
Chris Patuzzo <chris@patuzzo.co.uk>
2021-10-31 17:57:34 +0000

    Update the artwork and simplify the MintChip’s placement
    Always place the poker chip over the card outline. I plan to make it so
    the same mechanism is used (clicking on the metamask button) to mint
    chips after the user has selected the number of cards and tier on the
    back of the poker chip.

--- components/MintChip/index.jsx
+++ components/MintChip/index.jsx
@@ -1,16 +1,13 @@
-import { useState, useEffect } from "react";
+import { useContext, useRef, useState, useEffect } from "react";
+import AppContext from "../AppRoot/context"
 import Draggable from "../Draggable";
 import Zoomable from "../Zoomable";
 import Flippable from "../Flippable";
 import styles from "./styles.module.scss";
 
 const MintChip = ({ filters }) => {
+  const { PuzzleCard } = useContext(AppContext);
   const [zoomed, setZoomed] = useState(false);
-  const [loaded, setLoaded] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => setLoaded(true), 1200);
-  }, []);
 
   const zoomIn = () => {
     setZoomed(true);
@@ -20,21 +17,34 @@ const MintChip = ({ filters }) => {
     }, 0);
   };
 
-  const zoomOut = () => {
+  const zoomOut = (event) => {
     removeEventListener("mousedown", zoomOut);
     removeEventListener("scroll", zoomOut);
     setZoomed(false);
   };
 
-  const rotation = { base: 0, random: 90 };
+  const rotation = { base: 0, random: 30, initial: -20 };
   const slidersOffScreen = filters.deck.length <= 6;
 
   return (
-    <Draggable bounds="parent" zoomed={zoomed} onClick={zoomIn} disabled={zoomed} className={`${styles.draggable} ${loaded && styles.loaded} ${slidersOffScreen && styles.sliders_off_screen}`}>
+    <Draggable bounds="parent" zoomed={zoomed} onClick={zoomIn} disabled={zoomed} className={`${styles.draggable} ${slidersOffScreen && styles.sliders_off_screen}`}>
       <Zoomable zoomed={zoomed} rotateWhenZoomedOut={true} rotation={rotation} duration={1.5} minWidth={800} minHeight={800}>
         <Flippable flipped={zoomed} direction={Math.random() < 0.5 ? 1 : -1} delay={zoomed ? 0 : 0.5} className={styles.flippable}>
-          <div className={styles.front}></div>
-          <div className={styles.back}></div>
+          <div className={styles.front}>
+            <img src={`/images/poker_chip_black.png`} />
+            <div className={`${styles.content} ${styles.dark_mode}`}>
+              <span className={styles.mint}>Mint</span>
+              <span className={styles.price}>$0.01</span>
+            </div>
+          </div>
+
+          <div className={styles.back}>
+            <img src={`/images/poker_chip_white.png`} />
+            <div className={styles.content}>
+              <span className={styles.mint}>Mint</span>
+              <span className={styles.price}>$0.01</span>
+            </div>
+          </div>
         </Flippable>
       </Zoomable>
     </Draggable>
