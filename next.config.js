1987404cac4f8fad15fb0335727354218a30d762
Chris Patuzzo <chris@patuzzo.co.uk>
2021-11-29 16:27:38 +0000

    Add CORS headers in development so that it matches production

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 const { withSentryConfig } = require("@sentry/nextjs");
 
-module.exports = withSentryConfig({}, { silent: true });
+module.exports = withSentryConfig({
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+        ],
+      },
+    ]
+  },
+}, { silent: true });
