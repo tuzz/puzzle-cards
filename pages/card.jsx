7ac811567dafdf12ffe0a230a78929018c38cb98
Chris Patuzzo <chris@patuzzo.co.uk>
2021-11-29 17:12:31 +0000

    Add a CardViewer component
    I plan to use this for both embedding iframes into OpenSea and as a full
    screen link a user can visit.

--- pages/card.jsx
+++ pages/card.jsx
@@ -1,36 +1,15 @@
 import { useRouter } from "next/router"
-import CardFront from "../components/CardFront";
 import PuzzleCard from "../public/PuzzleCard";
+import CardViewer from "../components/CardViewer";
 
-const Tmp = () => {
+const CardPage = () => {
   const router = useRouter();
   const { tokenID } = router.query;
 
   if (!tokenID) { return null; }
-  //const card = PuzzleCard.fromTokenID(BigInt(tokenID));
-  const card = new PuzzleCard({
-    series: "Teamwork",
-    puzzle: "II",
-    tier: "Master",
-    type: "Artwork",
-    color1: "None",
-    color2: "None",
-    variant: "Anglerfish",
-    condition: "Pristine",
-    edition: "Limited",
-  });
+  const card = PuzzleCard.fromTokenID(BigInt(tokenID));
 
-  return (
-    <div style={{ position: "absolute", top: 0, bottom: 0, left: 0, right: 0, background: "#999" }}>
-      <div style={{ width: "45rem", height: "63rem", margin: "auto", marginTop: "5rem", position: "relative" }}>
-        <CardFront card={card} />
-      </div>
-
-      <div style={{ width: "15rem", height: "21rem", margin: "auto", marginTop: "5rem", position: "absolute", left: "10rem", top: "5rem" }}>
-        <CardFront card={card} />
-      </div>
-    </div>
-  );
+  return <CardViewer card={card} />;
 };
 
-export default Tmp;
+export default CardPage;
