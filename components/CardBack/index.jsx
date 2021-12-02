cc2da5fc2133c8db75b2e5746b74444076cac25b
Chris Patuzzo <chris@patuzzo.co.uk>
2021-12-02 17:19:23 +0000

    Design the back of the cards

--- components/CardBack/index.jsx
+++ components/CardBack/index.jsx
@@ -1,3 +1,4 @@
+import VectorText from "../VectorText";
 import styles from "./styles.module.scss";
 
 const CardBack = ({ defects, isMasterCopy }) => {
@@ -6,12 +7,27 @@ const CardBack = ({ defects, isMasterCopy }) => {
 
   const classes = [
     styles.card_back,
-    styles[`${defects.folded_corner}_folded_corner`],
+    defects.folded_corner && styles[`clip_${defects.folded_corner.corner}`],
     isMasterCopy && styles.master_copy,
   ].filter(s => s).join(" ");
 
   return (
-    <div className={classes}></div>
+    <div className={classes}>
+      <div className={styles.inner}>
+        <VectorText className={styles.worship} text="Worship" />
+        <VectorText className={styles.the_sun} text="the Sun" />
+
+
+        <img className={styles.bottom_sun} src="/images/yellow_sun.png" />
+        <img className={styles.left_sun} src="/images/yellow_sun.png" />
+        <img className={styles.right_sun} src="/images/yellow_sun.png" />
+
+        <span className={styles.puzzle_cards}>
+          <VectorText className={styles.puzzle} text="Puzzle" />
+          <VectorText className={styles.cards} text="Cards" />
+        </span>
+      </div>
+    </div>
   );
 };
 
