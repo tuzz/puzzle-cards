6a9f7f58d200aaa72a13f52797e3aabae933cdf4
Chris Patuzzo <chris@patuzzo.co.uk>
2021-12-18 19:00:54 +0000

    Update the scripts to deploy the static website

--- components/AppRoot/index.jsx
+++ components/AppRoot/index.jsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 const AppRoot = ({ Component, pageProps }) => <>
   <Head>
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    <title>Puzzle Cards</title>
   </Head>
   <Component {...pageProps} />
 </>;
