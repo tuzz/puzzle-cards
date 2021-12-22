86d2eb86236c2709b8f77f71f573ca8e65a34ce0
Chris Patuzzo <chris@patuzzo.co.uk>
2021-12-22 19:01:57 +0000

    Add social media meta tags

--- components/AppRoot/index.jsx
+++ components/AppRoot/index.jsx
@@ -1,11 +1,37 @@
+import { useRouter } from "next/router"
 import Head from "next/head";
 
-const AppRoot = ({ Component, pageProps }) => <>
-  <Head>
-    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    <title>Puzzle Cards</title>
-  </Head>
-  <Component {...pageProps} />
-</>;
+const AppRoot = ({ Component, pageProps }) => {
+  const router = useRouter();
+
+  return <>
+    <Head>
+      <title>Puzzle Cards</title>
+
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="keywords" content="Puzzle Cards,Worship the Sun,NFT,Chris Patuzzo,puzzle,platform,game,card game" />
+      <meta name="description" content="An original card game by Chris Patuzzo." />
+      <meta name="author" content="Chris Patuzzo" />
+
+      <meta property="og:title" content="Worship the Sun Puzzle Cards" />
+      <meta property="og:url" content={`https://puzzlecards.github.io${router.pathname}`} />
+      <meta property="og:type" content="website" />
+      <meta property="og:description" content="An original card game by Chris Patuzzo." />
+      <meta property="og:image" content="https://puzzlecards.github.io/images/facebook_share.png" />
+      <meta property="og:image:secure_url" content="https://puzzlecards.github.io/images/facebook_share.png" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="Worship the Sun Puzzle Cards" />
+      <meta name="twitter:site" content="@puzzle_cards" />
+      <meta name="twitter:creator" content="@chrispatuzzo" />
+      <meta name="twitter:description" content="An original card game by Chris Patuzzo." />
+      <meta name="twitter:image" content="https://puzzlecards.github.io/images/twitter_share.png" />
+    </Head>
+
+    <Component {...pageProps} />
+  </>;
+};
 
 export default AppRoot;
